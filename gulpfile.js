731766667ea2ca0295f864b72e6f7ca1ca3c664c
Ramil <vafarulez@ya.ru>
2017-04-01 19:47:59 +0300

    Транспиллер Stylus в gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@
 
 var gulp = require('gulp')
 var pug = require('gulp-pug')
+var stylus = require('gulp-stylus')
 var del = require('del')
 var browserSync = require('browser-sync').create()
 
 var path = {
     templates: 'src/templates/',
-    pages: 'dist/'
+    styles: 'src/styles/',
+    dist: {
+        pages: 'dist/',
+        styles: 'dist/styles/',
+    }
 }
 
 gulp.task('templates', function() {
@@ -15,7 +20,14 @@ gulp.task('templates', function() {
     .pipe(pug({
 		pretty: true
 	}))
-    .pipe(gulp.dest(path.pages))
+    .pipe(gulp.dest(path.dist.pages))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('styles', function() {
+    return gulp.src(path.styles + 'main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest(path.dist.styles))
     .pipe(browserSync.stream())
 })
 
@@ -27,19 +39,21 @@ gulp.task('server', function() {
         port: 3001,
         host: 'localhost',
         logPrefix: 'frontend',
-        reloadDelay: 3000
+        open: false
+        // reloadDelay: 3000
     })
 })
 
 gulp.task('removedist', function() {
-    return del.sync(path.pages)
+    return del.sync(path.dist.pages)
 })
 
 gulp.task('watch', function() {
-    gulp.watch(path.templates + '*.pug', ['templates'])
+    gulp.watch(path.templates + '**/*.pug', ['templates'])
+    gulp.watch(path.styles + '**/*.styl', ['styles'])
     browserSync.reload()
 })
 
-gulp.task('build', ['removedist', 'templates'])
+gulp.task('build', ['removedist', 'templates', 'styles'])
 
-gulp.task('default', ['templates', 'server', 'watch'])
+gulp.task('default', ['templates', 'styles', 'server', 'watch'])
