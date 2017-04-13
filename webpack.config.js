fe69be72758fe9241b5efbc4efb0edd86753a5eb
rtxrulez <vafarulez@ya.ru>
2017-04-13 17:08:26 +0300

    modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-let webpack = require('webpack');
+'use strict';
+const webpack = require('webpack');
 let path = {
     outputDir: __dirname + '/dist/js',
-    outputFile 'bundle.js'
+    outputFile: 'bundle.js'
 };
+
+// берем переменную окружения
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
     context: __dirname,
-    devtool: 'source-map',
-    entry: './src/js/app.js',
+    entry: './src/js/app.js', // входной скрипт
+    watch: NODE_ENV == 'development', // вотчер только для разработки
+    watchOptions: {
+        aggregateTimeout: 100 // задержка перед обработкой
+    },
+    devtool: NODE_ENV == 'development' ? 'source-map' : null, // sourcemap только для разработки
     output: {
         path: path.outputDir,
         filename: path.outputFile
     },
-    
     module: {
         loaders: [
             {
+                test: /\.(pug|jade)$/,
+                loader: 'pug-loader'
+            },
+            {
                 test: /\.js/,
-                loader: 'babel-loader',
+                loader: 'babel-loader?presets[]=es2015',
                 exclude: /(node_modules|bower_components)/
             },
             {
